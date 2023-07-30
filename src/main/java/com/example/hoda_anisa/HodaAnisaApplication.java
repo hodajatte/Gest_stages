9d3faa5d3040937c5b808a9364227c1b3db22f86
hodajatte <88599603+hodajatte@users.noreply.github.com>
2023-07-31 00:50:09 +0100

    Initial commit

--- src/main/java/com/example/hoda_anisa/HodaAnisaApplication.java
+++ src/main/java/com/example/hoda_anisa/HodaAnisaApplication.java
@@ -2,6 +2,9 @@ package com.example.hoda_anisa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class HodaAnisaApplication {
@@ -9,5 +12,8 @@ public class HodaAnisaApplication {
     public static void main(String[] args) {
         SpringApplication.run(HodaAnisaApplication.class, args);
     }
-
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
