e8f21ccc79f5e087e7c7f7ff646ec46e87011338
hodajatte <88599603+hodajatte@users.noreply.github.com>
2023-07-31 00:35:43 +0100

    Initial commit

--- src/main/java/com/example/hoda_anisa/Config/SecurityConfig.java
+++ src/main/java/com/example/hoda_anisa/Config/SecurityConfig.java
@@ -1,18 +1,35 @@
 package com.example.hoda_anisa.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        return new InMemoryUserDetailsManager(User.withUsername("user1").password("{noop}2000").roles("ADMIN").build(),
+                User.withUsername("user2").password("{noop}123").roles("Stagiaires").build(),
+                User.withUsername("user3").password("1234").roles("USER","ADMIN").build()
+                // Si on veut pas utiliser encoder on fait {noop}
+                // User.withUsername("user1").password("{noop}2000").roles("ADMIN").build(),
+
+
+        );
+    }
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
         httpSecurity.formLogin();
         httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
-
                 return httpSecurity.build();
 
     }
+
 }
